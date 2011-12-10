3f595490759c93ec2198c2cebc5c406e56da4b25
Patrick Mahoney <pat@polycrystal.org>
2011-12-10 12:36:45 -0600

    Remove test code from library file.

--- lib/gsl.rb
+++ lib/gsl.rb
@@ -1,6 +1,7 @@
 require 'ffi'
 
 require 'gsl/gsl'
+require 'gsl/matrix'
 require 'gsl/matrix/char'
 require 'gsl/matrix/complex'
 require 'gsl/matrix/complex/float'
@@ -16,51 +17,9 @@ require 'gsl/matrix/ulong'
 require 'gsl/matrix/uchar'
 require 'gsl/matrix/ushort'
 
+
 #require 'gsl/error'
 #require 'gsl/vector'
 #require 'gsl/matrix'
 
 require 'gsl/symbols'
-
-module GSL
-  SYMBOLS.each do |sym|
-    @routes.each do |rr|
-      regex, receiver = rr
-      if match = regex.match(sym)
-        receiver.call(sym, match, self)
-      end
-    end if @routes
-  end
-end
-
-3.times {
-  GC.start
-
-  [GSL::Matrix,
-   GSL::Matrix::Char,
-   GSL::Matrix::Complex,
-   GSL::Matrix::Complex::Float,
-#   GSL::Matrix::Complex::LongDouble,
-   GSL::Matrix::Float,
-   GSL::Matrix::Int,
-#   GSL::Matrix::LongDouble,
-   GSL::Matrix::Long,
-   GSL::Matrix::Short,
-   GSL::Matrix::UChar,
-   GSL::Matrix::UInt,
-   GSL::Matrix::ULong,
-   GSL::Matrix::UShort].each do |klass|
-
-    m1 = klass.new(10, 10)
-    m2 = klass.new(10, 10)
-
-    m1.add! m2
-    
-    puts m1
-    m1 = nil
-  end
-}
-
-abort 'goodbye'
-  
-
